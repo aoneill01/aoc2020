73a29064245a243951f00b7712a86d1bbc1be034
Andy O'Neill <aoneill01@gmail.com>
2020-12-20 16:17:32 -0500

    Day 20, Part 2

--- day20.js
+++ day20.js
@@ -9,7 +9,7 @@ const directions = {
   left: 3,
 }
 
-class Piece {
+class Tile {
   constructor(data) {
     const [first, ...rest] = data.split('\n')
     this.id = parseInt(first.match(/Tile (\d+):/)[1])
@@ -49,7 +49,7 @@ class Piece {
     this.image.reverse()
   }
 
-  // Returns true if this piece can be rotated to match the inputs. If it is a match, the piece is rotated correctly
+  // Returns true if this tile can be rotated to match the inputs. If it is a match, the tile is rotated correctly
   matches(topEdge, rightEdge, bottomEdge, leftEdge) {
     // Try four rotations
     for (let i = 0; i < 4; i++) {
@@ -74,13 +74,14 @@ class Piece {
   }
 }
 
-const [firstPiece, ...otherPieces] = input.map((data) => new Piece(data))
-const unplacedMap = otherPieces.reduce((acc, piece) => acc.set(piece.id, piece), new Map())
+// Part 1
+const [firstTile, ...otherTiles] = input.map((data) => new Tile(data))
+const unplacedMap = otherTiles.reduce((acc, tile) => acc.set(tile.id, tile), new Map())
 
-// Build an array will contained the placed pieces. Keep an outer edge of nulls.
+// Build an array will contained the placed tiles. Keep an outer edge of nulls.
 const result = [
   [null, null, null],
-  [null, firstPiece, null],
+  [null, firstTile, null],
   [null, null, null],
 ]
 
@@ -105,29 +106,27 @@ while (unplacedMap.size > 0) {
       if (topEdge === undefined && rightEdge === undefined && bottomEdge === undefined && leftEdge === undefined) {
         continue
       }
-      // Find pieces that will work
-      const matches = [...unplacedMap.values()].filter((piece) =>
-        piece.matches(topEdge, rightEdge, bottomEdge, leftEdge)
-      )
+      // Find tiles that will work
+      const matches = [...unplacedMap.values()].filter((tile) => tile.matches(topEdge, rightEdge, bottomEdge, leftEdge))
       if (matches.length === 1) {
-        // Found a piece
+        // Found a tile
         unplacedMap.delete(matches[0].id)
         result[row][col] = matches[0]
       }
     }
   }
 
-  // Update the edges if we placed any pieces there
+  // Update the edges if we placed any tiles there
   if (result.some((row) => row[0])) {
     result.forEach((row) => row.unshift(null))
   }
   if (result.some((row) => row[row.length - 1])) {
     result.forEach((row) => row.push(null))
   }
-  if (result[0].some((piece) => piece)) {
+  if (result[0].some((tile) => tile)) {
     result.unshift(result[0].map(() => null))
   }
-  if (result[result.length - 1].some((piece) => piece)) {
+  if (result[result.length - 1].some((tile) => tile)) {
     result.push(result[result.length - 1].map(() => null))
   }
 }
@@ -138,3 +137,97 @@ const part1 =
   result[1][result[1].length - 2].id *
   result[result.length - 2][result[result.length - 2].length - 2].id
 console.log('Part 1', part1)
+
+// Part 2
+const image = []
+for (let tileRowIndex = 1; tileRowIndex < result.length - 1; tileRowIndex++) {
+  const firstTile = result[tileRowIndex][1]
+  for (let rowIndex = 1; rowIndex < firstTile.image.length - 1; rowIndex++) {
+    const row = []
+    image.push(row)
+    for (let tileColIndex = 1; tileColIndex < result[tileRowIndex].length - 1; tileColIndex++) {
+      const tile = result[tileRowIndex][tileColIndex]
+      for (let colIndex = 1; colIndex < tile.image[rowIndex].length - 1; colIndex++) {
+        row.push(tile.image[rowIndex][colIndex])
+      }
+    }
+  }
+}
+
+// Convert to a tile for easy transformations
+const sateliteTile = new Tile('Tile 42:\n' + image.map((row) => row.join('')).join('\n'))
+
+const seaMonster = `                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   `
+  .split('\n')
+  .map((row) => row.split(''))
+
+// Find the correct orientation that has sea monsters
+const rotateTransformation = (tile) => tile.rotate()
+const flipTransformation = (tile) => tile.flip()
+const transformations = [
+  rotateTransformation,
+  rotateTransformation,
+  rotateTransformation,
+  rotateTransformation,
+  flipTransformation,
+  rotateTransformation,
+  rotateTransformation,
+  rotateTransformation,
+]
+for (const transformation of transformations) {
+  if (hasSeaMonster(sateliteTile)) {
+    break
+  }
+  transformation(sateliteTile)
+}
+
+function hasSeaMonster(sateliteTile) {
+  for (let row = 0; row < sateliteTile.image.length; row++) {
+    for (let col = 0; col < sateliteTile.image[row].length; col++) {
+      if (isSeaMonsterAt(row, col, sateliteTile)) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+function isSeaMonsterAt(row, col, sateliteTile) {
+  if (
+    row + seaMonster.length >= sateliteTile.image.length ||
+    col + seaMonster[0].length >= sateliteTile.image[0].length
+  ) {
+    return false
+  }
+  for (let rowDelta = 0; rowDelta < seaMonster.length; rowDelta++) {
+    for (let colDelta = 0; colDelta < seaMonster[rowDelta].length; colDelta++) {
+      if (seaMonster[rowDelta][colDelta] === '#' && sateliteTile.image[row + rowDelta][col + colDelta] === '.') {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+function markSeaMonsterAt(row, col, sateliteTile) {
+  for (let rowDelta = 0; rowDelta < seaMonster.length; rowDelta++) {
+    for (let colDelta = 0; colDelta < seaMonster[rowDelta].length; colDelta++) {
+      if (seaMonster[rowDelta][colDelta] === '#' && sateliteTile.image[row + rowDelta][col + colDelta] !== '.') {
+        sateliteTile.image[row + rowDelta][col + colDelta] = 'O'
+      }
+    }
+  }
+}
+
+for (let row = 0; row < image.length; row++) {
+  for (let col = 0; col < image[row].length; col++) {
+    if (isSeaMonsterAt(row, col, sateliteTile)) {
+      markSeaMonsterAt(row, col, sateliteTile)
+    }
+  }
+}
+
+const part2 = sateliteTile.image.flat().reduce((acc, cell) => acc + (cell === '#' ? 1 : 0), 0)
+console.log('Part 2', part2)
